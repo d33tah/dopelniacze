fe0cf283ba7b7295a6cf56f5d6f5c0d4183f48f5
Jacek "d33tah" Wielemborek <git@d33.pl>
2024-12-21 17:35:46 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,108 +1,97 @@
 # Przepisz mi to na Fleta
 
-"""
-
-Equivalent of the following program, but in flet:
-
-from flask import Flask, render_template_string, request, redirect, url_for
+import flet as ft
 import random
 import json
-
-app = Flask(__name__)
 
 # Załaduj dane do pamięci
 with open('dopelniacze.json', 'r', encoding='utf-8') as f:
     data = [json.loads(line) for line in f]
 
+class DopelniaczeApp(ft.UserControl):
+    def __init__(self):
+        super().__init__()
+        self.title = ft.Text()
 
-def handle_input():
+        self.input_lp = ft.TextField(label="Nie mam żadnej/żadnego:")
+        self.input_lm = ft.TextField(label="Nie mam żadnych:")
+        self.result = ft.Text()
+        self.btn_submit = ft.ElevatedButton("Sprawdź", on_click=self.handle_input)
+        self.score = 0
+        self.num_questions = 0
+        self.score_text = ft.Text()
+        self.spacer = ft.Container(height=50)
 
-    LINK = '''
-           <a href="/">Spróbuj ponownie</a>
-    '''
+        # Layout to arrange components vertically
+        self.layout = ft.Column(
+            controls=[
+                self.spacer,
+                self.score_text,
+                self.result,
+                self.title,
+                self.input_lp,
+                self.input_lm,
+                self.btn_submit,
+            ],
+            alignment=ft.MainAxisAlignment.START,
+            spacing=10
+        )
+        self.controls.append(self.layout)
 
-    user_dopelniacz_lp = request.form.get('dopelniacz_lp')
-    user_dopelniacz_lm = request.form.get('dopelniacz_lm')
-    correct_lp = request.form.get('correct_lp')
-    correct_lm = request.form.get('correct_lm')
+    def did_mount(self):
+        self.generate_page()
 
-    lp_correct = not bool(correct_lp) or user_dopelniacz_lp == correct_lp
-    lm_correct = not bool(correct_lm) or user_dopelniacz_lm == correct_lm
+    def handle_input(self, event):
+        self.num_questions += 1
+        user_dopelniacz_lp = self.input_lp.value
+        user_dopelniacz_lm = self.input_lm.value
+        correct_lp = self.correct_lp
+        correct_lm = self.correct_lm
 
-    result = ''
+        lp_correct = not bool(correct_lp) or user_dopelniacz_lp == correct_lp
+        lm_correct = not bool(correct_lm) or user_dopelniacz_lm == correct_lm
 
-    if lp_correct and lm_correct:
-        result += 'Brawo! Odpowiedź <font color="green">poprawna</font>.' + LINK
-    else:
-        result += 'Niestety, odpowiedź <font color="red">niepoprawna</font>. Poprawna odpowiedź to: <br>'
-        result += f'Nie mam żadnej/żadnego: {correct_lp!r}<br>'
-        result += f'Nie mam żadnych: {correct_lm!r}<br>'
-        result += 'Twoja odpowiedź: <br><font color="red">'
-        result += f'Nie mam żadnej/żadnego: {user_dopelniacz_lp}<br>'
-        result += f'Nie mam żadnych: {user_dopelniacz_lm}<br></font>'
-        result += LINK
+        if lp_correct and lm_correct:
+            self.result.value = 'Brawo! Odpowiedź poprawna.'
+            self.result.color = 'green'
+            self.score += 1
+        else:
+            self.result.value = (
+                f'Niestety, odpowiedź niepoprawna. Poprawna odpowiedź to:\n'
+                f'Nie mam żadnej/żadnego: {correct_lp}\n'
+                f'Nie mam żadnych: {correct_lm}\n'
+            )
+            self.result.color = 'red'
 
-    return result
+        self.update()
+        self.generate_page()
 
+    def update_score_text(self):
+        # calculate score, display percentage too
+        score_text = f'Wynik: {self.score}/{self.num_questions}'
+        if self.num_questions > 0:
+            score_text += f' ({self.score / self.num_questions:.0%})'
+        self.score_text.value = score_text
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        return handle_input()
-    else:
-        return generate_page()
+    def generate_page(self, event=None):
+        item = random.choice(data)
+        self.title.value = item['title']
+        self.correct_lp = item['declensions'].get('dopelniacz_lp', '')
+        self.correct_lm = item['declensions'].get('dopelniacz_lm', '')
 
-def generate_page():
+        # reset inputs
+        self.input_lp.value = ''
+        self.input_lm.value = ''
 
-    TEMPLATE = '''
-    <!doctype html>
-    <html lang="pl">
-      <head>
-        <meta charset="utf-8">
-        <title>Dopełniacze</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      </head>
-      <body>
-        <h2>{{ title }}</h2>
-        <form method="post">
-          <div>
-            <label>Nie mam żadnej/żadnego:</label>
-            <input type="text" name="dopelniacz_lp" required>
-          </div>
-          <div>
-            <label>Nie mam żadnych:</label>
-            <input type="text" name="dopelniacz_lm" required>
-          </div>
-          <input type="hidden" name="correct_lp" value="{{ dopelniacz_lp }}">
-          <input type="hidden" name="correct_lm" value="{{ dopelniacz_lm }}">
-          <button type="submit">Sprawdź</button>
-        </form>
-      </body>
-    </html>
-    '''
-
-    item = random.choice(data)
-    title = item['title']
-    dopelniacz_lp = item['declensions'].get('dopelniacz_lp', '')
-    dopelniacz_lm = item['declensions'].get('dopelniacz_lm', '')
-
-    return render_template_string(TEMPLATE, title=title, dopelniacz_lp=dopelniacz_lp, dopelniacz_lm=dopelniacz_lm)
-
-"""
-
-# Póki co mam tyle:
-
-import flet as ft
-
-class DopelniaczeApp(ft.Column):
-    pass
+        self.update_score_text()
+        self.update()
 
 def main(page: ft.Page):
-    page.title = "Dopelniacze"
+    page.title = "Dopełniacze"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.scroll = ft.ScrollMode.ADAPTIVE
 
     # create app control and add it to the page
     page.add(DopelniaczeApp())
 
-ft.app(main)
+ft.app(target=main, assets_dir="assets", view=ft.AppView.WEB_BROWSER)
